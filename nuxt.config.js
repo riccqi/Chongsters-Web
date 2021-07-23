73e7a18a1843e7f7b091fbc59eace2b19cb9c2e0
riccqi <xiaochean@gmail.com>
2021-07-24 01:30:19 +0800

    heroku settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/myplugin.client.js' }],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
